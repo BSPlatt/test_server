23db90b206397a5fb1805d73265ffa031ae8a0f1
Brad Platt <bradleysplatt@gmail.com>
2023-06-29 10:35:58 -0600

    fix ip address binding

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use actix_files::NamedFile;
 use actix_web::{
-    http::{Uri},
+    http::Uri,
     middleware::{self, TrailingSlash},
-    web, App, HttpServer
+    web, App, HttpServer,
 };
 use env_logger::Env;
 use std::time::Duration;
@@ -20,21 +20,18 @@ async fn static_file(uri: Uri) -> std::io::Result<NamedFile> {
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     env_logger::init_from_env(Env::default().default_filter_or("info"));
-    // env_logger::init_from_env("info");
-    // env_logger::init();
 
-    let host = 8081;
+    let host = 8080;
     println!("Server starting at http://localhost:{}", host);
     HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(index))
-            .route("/{pathy}", web::get().to(static_file))
+            .route("/{any_path}", web::get().to(static_file))
             .wrap(middleware::NormalizePath::new(TrailingSlash::Trim))
             .wrap(middleware::Logger::new("%s %r %a %D")) // Keep logger at end
-                                                          // .wrap(middleware::NormalizePath::new(TrailingSlash::Trim))
     })
     .keep_alive(Duration::from_secs(60))
-    .bind(("127.0.0.1", host))?
+    .bind(("0.0.0.0", host))?
     .run()
     .await
 }
