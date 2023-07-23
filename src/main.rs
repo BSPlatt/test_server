fc1f3b897471fb9ceceda14bd1225010b897b247
EC2 Default User <ec2-user@ip-172-31-90-175.ec2.internal>
2023-07-23 00:09:58 +0000

    adjustments for testing

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,17 @@ use actix_files::NamedFile;
 use actix_web::{
     http::Uri,
     middleware::{self, TrailingSlash},
-    web, App, HttpServer,
+    web, App, HttpServer,HttpResponse,
 };
 use env_logger::Env;
 use std::time::Duration;
 
-async fn index() -> std::io::Result<NamedFile> {
-    Ok(NamedFile::open("./static/index.html")?)
+//async fn index() -> std::io::Result<NamedFile> {
+//    Ok(NamedFile::open("./static/index.html")?)
+//}
+
+async fn index() -> HttpResponse {
+    HttpResponse::Ok().finish()
 }
 
 async fn static_file(uri: Uri) -> std::io::Result<NamedFile> {
@@ -19,8 +23,6 @@ async fn static_file(uri: Uri) -> std::io::Result<NamedFile> {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    env_logger::init_from_env(Env::default().default_filter_or("info"));
-
     let host = 8080;
     println!("Server starting at http://localhost:{}", host);
     HttpServer::new(|| {
@@ -28,7 +30,6 @@ async fn main() -> std::io::Result<()> {
             .route("/", web::get().to(index))
             .route("/{any_path}", web::get().to(static_file))
             .wrap(middleware::NormalizePath::new(TrailingSlash::Trim))
-            .wrap(middleware::Logger::new("%s %r %a %D")) // Keep logger at end
     })
     .keep_alive(Duration::from_secs(60))
     .bind(("0.0.0.0", host))?
